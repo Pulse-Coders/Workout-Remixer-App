931e2cfcb3e95d2c444eb3d715f7c4a7396e8dd0
Stephen Seeraj <stephenseeraj04@gmail.com>
2026-04-13 21:47:43 -0400

    fixed seeding with db in seed.py

--- app/utilities/seed.py
+++ app/utilities/seed.py
@@ -1,9 +1,14 @@
+from sqlmodel import Session, SQLModel
+from app.database import engine 
 
-from sqlmodel import Session
-from app.dependencies.session import engine # Ensure this path matches our engine location
-from app.models.workout import Workout
+from app.models.user import User 
+
+from app.models.workout import Workout, Routine, RoutineWorkoutLink
 
 def seed_database():
+    print("Building database tables...")
+    SQLModel.metadata.create_all(engine)
+    
     initial_workouts = [
         Workout(
             name="Standard Push-up", 
@@ -20,26 +25,55 @@ def seed_database():
             instructions="Lower bar to mid-chest, press up powerfully."
         ),
         Workout(
-            name="Dumbbell Row", 
-            muscle_group="Back", 
+            name="Dumbbell Flyes", 
+            muscle_group="Chest", 
             equipment="Dumbbells", 
-            difficulty="Beginner", 
-            instructions="Pull dumbbell to hip, keeping back straight."
+            difficulty="Intermediate", 
+            instructions="Slight bend in elbows, lower weights out to sides."
         ),
+ 
         Workout(
             name="Bodyweight Squat", 
             muscle_group="Legs", 
             equipment="Bodyweight", 
             difficulty="Beginner", 
             instructions="Keep chest up, push hips back and down."
+        ),
+        Workout(
+            name="Dumbbell Lunge", 
+            muscle_group="Legs", 
+            equipment="Dumbbells", 
+            difficulty="Intermediate", 
+            instructions="Step forward, lower hips until both knees are bent at 90 degrees."
+        ),
+
+        Workout(
+            name="Pull-up", 
+            muscle_group="Back", 
+            equipment="Bodyweight", 
+            difficulty="Advanced", 
+            instructions="Grip bar overhead, pull body up until chin clears the bar."
+        ),
+        Workout(
+            name="Dumbbell Row", 
+            muscle_group="Back", 
+            equipment="Dumbbells", 
+            difficulty="Beginner", 
+            instructions="Pull dumbbell to hip, keeping back straight."
         )
     ]
     
+    print("Injecting default workouts...")
     with Session(engine) as session:
         for workout in initial_workouts:
             session.add(workout)
-        session.commit()
-        print("✅ Database successfully seeded with default workouts!")
+            
+        try:
+            session.commit()
+            print("Database successfully seeded with default workouts!")
+        except Exception as e:
+            session.rollback()
+            print(f"Something went wrong: {e}")
 
 if __name__ == "__main__":
     seed_database()
